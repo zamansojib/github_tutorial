4d32501dc2262707d421b40a708504e21089ac39
unknown <zamansojib95.gmail.com>
2025-03-08 15:36:01 +0600

    second commit

---
+++ tutorial2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+
+    printf("Hello! This is john wick\n");
+    printf("I am a retired hitman\n");
+    printf("Right Now I am learning C programming\n");
+
+
+
+
+    return 0;
+}
